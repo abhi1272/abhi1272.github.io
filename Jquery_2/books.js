b5c0aa30629b1ed8ad157b6ff7144df7df6e7d2b
Abhishek <abhishek.omex@gmail.com>
2019-08-14 02:27:09 +0530

    added new project Needs

---
+++ Jquery_2/books.js
@@ -0,0 +1,18 @@
+myBooks = [
+	{
+"start": "2018-03-01",
+"title": "All Day Event",
+"status": "recieved"
+},
+{
+"start": "2018-03-01",
+"title": "Science",
+"status": "pay"
+}
+
+]
+
+
+for (var i = 0; i<myBooks.length;i++){
+	console.log(myBooks[i].status)
+}
